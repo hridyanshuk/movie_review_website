4fec791f4c6adebef5e57e61131990c413a43cc3
Manan parihar <83576753+manan03@users.noreply.github.com>
2022-07-03 19:33:35 +0530

    om.jsx

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -1,6 +1,21 @@
 export default () => {
     return (
-        <div className="home">
+        <div className="main">
+             <div className="card-header">
+                    Top Picks
+             </div>
+             <div className="card-body">
+                <div className="card-container">
+                    <div className="col">
+                        <picture>
+
+                        </picture>
+                        <a href="#">
+
+                        </a>
+                    </div>
+                </div>
+             </div>
 
         </div>
     )
